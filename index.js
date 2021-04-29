9246f0c26138005657a69ea320c9ae3849071761
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-28 22:22:57 -0400

    commit tree d55a2e5c900911edad397b755f49987fbb4aa828

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching (array, matchName) {
+    const match = array.filter((name) => {
+        return name.toLowerCase() === matchName.toLowerCase()
+    })
+    return match
+}
+
+function fuzzyMatch (array, matchName) {
+    const match = array.filter((name) => {
+        return name.slice(0, matchName.length) === matchName
+    })
+    return match
+}
+
+function matchName (arrayOfObjects, matchName) {
+    const match = arrayOfObjects.filter((object) => {
+        return object.name === matchName
+    })
+    return match
+}
